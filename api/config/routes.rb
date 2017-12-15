2e54c958cf30e866cd3e641fb3a55b6983704ca7
Rafael Franco <rafaelfr@outlook.com>
2017-12-15 21:57:06 -0200

    Added outes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   scope :format => true, :constraints => { :format => 'json' } do
     post "user/sign_in" => "sessions#create"
     delete "user/sign_out" => "sessions#destroy"
+    resource :user, controller: 'user'
+    resources :users, controller: 'users'
   end
 end
