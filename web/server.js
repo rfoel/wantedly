8ac22bcff5bff6b9d3ebe5ddacf3fd989bfc94cd
Rafael Franco <rafaelfr@outlook.com>
2017-12-18 23:14:57 -0200

    Updated server.js

--- web/server.js
+++ web/server.js
@@ -7,9 +7,8 @@ const app = express()
 
 app.use(history({ verbose: true }))
 app.use(serveStatic(path.join(__dirname, "/dist")))
+app.set("port", process.env.PORT || 5000)
 
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log("Server started at http://localhost:5000")
+app.listen(app.get("port"), () => {
+  console.log("Node server is running on port " + app.get("port"))
 })
