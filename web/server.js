deac3ca0bc5815b647f1436c68773005c32bd862
Rafael Franco <rafaelfr@outlook.com>
2017-12-18 22:57:05 -0200

    Updated express server

--- web/server.js
+++ web/server.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const history = require("connect-history-api-fallback")
 const path = require("path")
 const serveStatic = require("serve-static")
-app = express()
-app.use(serveStatic(__dirname + "/dist"))
+
+const app = express()
+
+app.use(history({ verbose: true }))
+app.use(serveStatic(path.join(__dirname, "/dist")))
+
 const port = process.env.PORT || 5000
-app.listen(port)
-console.log("server started " + port)
+
+app.listen(port, () => {
+  console.log("Server started at http://localhost:5000")
+})
